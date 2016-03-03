6cbb832b59359bfa481baa29d4634b882342df45
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2016-03-03 15:40:59 -0300

    Query for all documents in a nollection

--- mongo_examples/query_data_with_node/index.js
+++ mongo_examples/query_data_with_node/index.js
@@ -4,3 +4,37 @@ var MongoClient = require('mongodb').MongoClient,
 	assert = require('assert'),
 	ObjectId = require('mongodb').ObjectID,
 	url = 'mongodb://localhost:27017/test';
+
+var findRestaurants = function(db, callback) {
+
+	var cursor = db.collection('restaurants').find();
+
+	cursor.each(function(error, doc) {
+
+		assert.equal(error, null);
+
+		if (doc) {
+
+			console.log(JSON.stringify(doc));
+
+		} else {
+
+			callback();
+
+		}
+
+
+	});
+
+};
+
+MongoClient.connect(url, function(error, db) {
+
+	assert.equal(error, null);
+
+	findRestaurants(db, function() {
+		console.log('Done!');
+		db.close();
+	});
+
+});
