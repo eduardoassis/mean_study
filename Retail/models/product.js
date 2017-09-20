eda8be70abf882b1e01e848f36366b78b756c2fe
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2017-09-19 22:43:23 -0300

    Creating virtuals to retrieve price with convenient currency property

--- Retail/models/product.js
+++ Retail/models/product.js
@@ -16,15 +16,30 @@ var productSchema = {
 		amount: {
 			type: Number,
 			required: true
+		},
+		currency: {
+			type: String,
+			enum: ['USD', 'EUR', 'GBP'],
+			required: true
 		}
-	},
-	currency: {
-		type: String,
-		enum: ['USD', 'EUR', 'GBP'],
-		required: true
 	},
 	category: Category.categorySchema
 };
 
-module.exports = new mongoose.Schema(productSchema);
+var schema = new mongoose.Schema(productSchema);
+
+var currencySymbols = {
+	'USD': '$',
+	'EUR': '€',
+	'GBP': '£'
+};
+
+schema.virtual('displayPrice').get(function(){
+	return currencySymbols[this.price.currency] + '' + this.price.amount; 
+});
+
+schema.set('toJSON', {virtuals : true});
+schema.set('toObject', {virtuals : true});
+
+module.exports = schema;
 module.exports.productSchema = productSchema;
